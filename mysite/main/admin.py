d7c9148e03e86a7117ef3795bfd19dfcf76d9c48
Hasham Akhtar <akhtar.hashm@gmail.com>
2022-12-21 12:24:55 +0500

    Certain changes made: 1)POST & GET info from the form 2)Navigation bar has been added

---
+++ mysite/main/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Todolist, Item
+admin.site.register(Todolist)
+admin.site.register(Item)
+
+# Register your models here.
---
+++ mysite/main/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Todolist, Item
+from .forms import CreateNewList
+# Create your views here.
+
+
+def index(response, id):
+    ls = Todolist.objects.get(id=id)
+    return render(response,"main/list.html", {"ls":ls})
+
+
+def home(response):
+    return render(response, "main/home.html", {})
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = Todolist(name=n)
+            t.save()
+    else:
+        form = CreateNewList()
+    return render(response, "main/create.html", {"form":form})
