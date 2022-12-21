6e6332079f667ef4cbfe9511423b6e413c4f1ccd
Hasham Akhtar <akhtar.hashm@gmail.com>
2022-12-21 12:33:05 +0500

    changes made: 1) POST and GET via form 2) Navigation bar added

--- mysite/main/views.py
+++ mysite/main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import Todolist, Item
 from .forms import CreateNewList
 # Create your views here.
@@ -7,6 +8,27 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = Todolist.objects.get(id=id)
+    
+    #{"save":["save"],"c1":["clicked"]}
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                
+                item.save()
+        
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+            
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("Invalid Input")  
+
     return render(response,"main/list.html", {"ls":ls})
 
 
@@ -21,6 +43,8 @@ def create(response):
             n = form.cleaned_data["name"]
             t = Todolist(name=n)
             t.save()
+        return HttpResponseRedirect("/%i" %t.id)
+    
     else:
         form = CreateNewList()
     return render(response, "main/create.html", {"form":form})
